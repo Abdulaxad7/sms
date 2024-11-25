8dba183776fbc11180799d74722da857653748fb
iso0426 <muhammadisojorayev27@gmail.com>
2024-11-25 17:11:06 +0500

    added label

--- src/main/java/com/sms/sms/HelloApplication.java
+++ src/main/java/com/sms/sms/HelloApplication.java
@@ -18,6 +18,9 @@ public class HelloApplication extends Application {
 
         // comment
         //muhsmmsdiso's comment
+
+        System.out.println("hello from iso");
+
     }
 
     public static void main(String[] args) {
