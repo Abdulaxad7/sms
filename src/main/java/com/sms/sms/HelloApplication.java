a70102ae3c2d06533f2dcaae1b99b6e3efc6c5e9
iso0426 <muhammadisojorayev27@gmail.com>
2024-11-25 17:03:20 +0500

    commits by muhammadiso

--- src/main/java/com/sms/sms/HelloApplication.java
+++ src/main/java/com/sms/sms/HelloApplication.java
@@ -17,6 +17,7 @@ public class HelloApplication extends Application {
         stage.show();
 
         // comment
+        //muhsmmsdiso's comment
     }
 
     public static void main(String[] args) {
